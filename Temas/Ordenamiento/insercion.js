e4f20deb1730576c33a91a0290998bd8f0624330
Emanuel Alvarado <emanuelalvaradog@gmail.com>
2022-03-19 15:24:23 -0600

    Ordenamiento

--- Temas/Ordenamiento/insercion.js
+++ Temas/Ordenamiento/insercion.js
@@ -1,16 +1,25 @@
-const generico = { dato };
-const dato = { generico };
+const Arreglo = [
+  { contenedor: { dato: 5 } },
+  { contenedor: { dato: 7 } },
+  { contenedor: { dato: 10 } },
+  { contenedor: { dato: 2 } },
+  { contenedor: { dato: 3 } },
+  { contenedor: { dato: 1 } },
+];
 
-function ordenar(arreglo) {
+function insercion(arreglo) {
   let pivote;
   let j;
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < arreglo.length; i++) {
     pivote = arreglo[i];
     j = i - 1;
-    while (j >= 0 && arreglo[j].generico.dato > pivote.generico.dato) {
+    while (j >= 0 && arreglo[j].contenedor.dato > pivote.contenedor.dato) {
       arreglo[j + 1] = arreglo[j];
       j--;
     }
     arreglo[j + 1] = pivote;
   }
+  return arreglo;
 }
+
+console.log(insercion(Arreglo));
--- Temas/Ordenamiento/seleccion.js
+++ Temas/Ordenamiento/seleccion.js
@@ -1,16 +1,23 @@
-const array = [{obj: {dato: 5}},{obj: {dato: 4}},{obj: {dato: 0}},{obj: {dato: 2}},{obj: {dato: 10}},{obj: {dato: 8}},]
+const array = [
+  { contenedor: { dato: 5 } },
+  { contenedor: { dato: 4 } },
+  { contenedor: { dato: 0 } },
+  { contenedor: { dato: 2 } },
+  { contenedor: { dato: 10 } },
+  { contenedor: { dato: 8 } },
+];
 
-function ordenamientoSeleccion(array, tam){
-    for(let i = 0; i<tam; i++){
-        for(let j = i+1; j<tam; j++){
-            if(array[j].obj.dato < array[i].obj.dato){
-                let a = array[i];
-                array[i] = array[j];
-                array[j] = a;
-            }
-        }
+function ordenamientoSeleccion(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j].contenedor.dato < array[i].contenedor.dato) {
+        let a = array[i];
+        array[i] = array[j];
+        array[j] = a;
+      }
     }
-    console.log(array)
+  }
+  return array;
 }
 
-ordenamientoSeleccion(array, array.length);
+console.log(ordenamientoSeleccion(array));
